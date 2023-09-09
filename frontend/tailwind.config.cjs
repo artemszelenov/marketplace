a8e0492de12e61d91609d66c5f2f96960d542f9a
Artem Zelenov <deeloyy@gmail.com>
2023-09-09 20:30:24 +0600

    style focus state

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -12,8 +13,41 @@ module.exports = {
 			fontFamily: {
 				sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
 				// serif: ['DM Serif Display', ...defaultTheme.fontFamily.serif]
-			},
+			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addComponents, addBase, theme }) => {
+			addBase({
+				'a[href], button': {
+					'&:focus': {
+						'outline': `1px dashed ${theme('colors.black')}`,
+						'outline-offset': '2px'
+					}
+				}
+			})
+
+			addComponents({
+				'.button-primary': {
+					'display': 'inline-flex',
+					'justify-content': 'center',
+					'align-items': 'center',
+					'gap': theme('spacing.2'),
+					'padding-left': theme('spacing.6'),
+					'padding-right': theme('spacing.6'),
+					'padding-top': theme('spacing.3'),
+					'padding-bottom': theme('spacing.3'),
+					'background-color': theme('colors.black'),
+					// 'color': theme('colors.white'), // разобраться почему не работает с цветом
+					'user-select': 'none',
+					'outline': 'none',
+					'border-color': theme('colors.black'),
+					'transition': 'outline-color 200ms ease',
+					'&:focus:not(:focus-visible)': {
+						'outline-color': `transparent`
+					}
+				}
+			})
+		})
+	]
 }
