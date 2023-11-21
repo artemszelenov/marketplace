d42faf1604c06f6010d40e0bed5ed8d7ce43b896
Artem Zelenov <deeloyy@gmail.com>
2023-11-21 13:01:33 +0600

    remove payloadcms

--- backend/src/access/admins.ts
+++
@@ -1,9 +0,0 @@
-import type { AccessArgs } from 'payload/config'
-import type { User } from '@/payload-types'
-import { checkRole } from '../collections/Users/checkRole'
-
-type isAdmin = (args: AccessArgs<any, User>) => boolean
-
-export const admins: isAdmin = ({ req: { user } }) => {
-  return checkRole(['admin'], user)
-}
