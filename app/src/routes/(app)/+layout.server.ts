3a06bc0d92aa4e9ed3cd2059f1938ce3bb5757ae
Artem Zelenov <artem.zelenov@icloud.com>
2025-02-27 14:06:41 +0600

    old version with checkout page

--- app/src/routes/(app)/+layout.server.ts
+++ app/src/routes/(app)/+layout.server.ts
@@ -1,24 +1,22 @@
 export async function load({ locals, depends, cookies }) {
-  depends("layout:root");
+	depends("layout:root");
 
-  const cart_id_cookie = cookies.get("pb_cart");
+	const cart_id_cookie = cookies.get("pb_cart");
 
-  let cart_items_count = 0;
-  try {
-    const cart_items = await locals.pb
-      .collection("cart_items")
-      .getFullList({
-        filter: `cart = "${cart_id_cookie}"`
-      });
+	let cart_items_count = 0;
+	try {
+		const cart_items = await locals.pb.collection("cart_items").getFullList({
+			filter: `cart = "${cart_id_cookie}"`,
+		});
 
-    cart_items_count = cart_items.length;
-  } catch (_) {}
+		cart_items_count = cart_items.length;
+	} catch (_) {}
 
-  return {
-    user: locals.user,
-    cart_items_count,
-    seo: {
-      title: "Главная"
-    }
-  }
+	return {
+		user: locals.user,
+		cart_items_count,
+		seo: {
+			title: "Главная",
+		},
+	};
 }
--- app/src/routes/checkout/+layout.server.ts
+++ app/src/routes/checkout/+layout.server.ts
@@ -5,42 +5,31 @@ export async function load({ fetch, cookies }) {
     throw redirect(303, "/");
   }
 
-  const cdek_access_token = await fetch(
-    "https://api.edu.cdek.ru/v2/oauth/token?client_id=EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI&client_secret=PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG&grant_type=client_credentials",
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-      },
-    }
-  )
-    .then((res) => res.json())
-    .then(({ access_token }) => access_token);
+  // const cdek_access_token = await fetch(
+  //   "https://api.edu.cdek.ru/v2/oauth/token?client_id=EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI&client_secret=PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG&grant_type=client_credentials",
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/x-www-form-urlencoded",
+  //     },
+  //   }
+  // )
+  //   .then((res) => res.json())
+  //   .then(({ access_token }) => access_token);
 
-  const cdek_cities: CdekCity[] = await fetch(
-    `https://api.edu.cdek.ru/v2/location/cities?lang=rus&country_codes=RU`,
-    {
-      headers: {
-        authorization: `Bearer ${cdek_access_token}`,
-      },
-    }
-  ).then((res) => res.json());
+  // const cdek_cities: CdekCity[] = await fetch(
+  //   `https://api.edu.cdek.ru/v2/location/cities?lang=rus&country_codes=RU`,
+  //   {
+  //     headers: {
+  //       authorization: `Bearer ${cdek_access_token}`,
+  //     },
+  //   }
+  // ).then((res) => res.json());
 
-  cookies.set("cdek_token", cdek_access_token, { path: "/" });
+  // cookies.set("cdek_token", cdek_access_token, { path: "/" });
 
   return {
-    cdek_cities: cdek_cities.sort((city_a, city_b) => {
-      const first_city_name = city_a.city.toLowerCase();
-      const second_city_name = city_b.city.toLowerCase();
-
-      if (first_city_name < second_city_name) {
-        return -1;
-      }
-      if (first_city_name > second_city_name) {
-        return 1;
-      }
-      return 0;
-    }),
+    cdek_cities: [],
     seo: {
       title: "Оформление заказа",
     },
